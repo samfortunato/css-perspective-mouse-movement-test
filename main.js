d70412440578307d0bd0e79898f9778d4f5c159d
Samuel Fortunato <samfortunato@gmail.com>
2019-03-21 21:33:35 -0400

    Make square change perspective on mousemouse; make square change color on mouse click

---
+++ main.js
@@ -0,0 +1,20 @@
+const perspectiveSquare = document.querySelector('#square');
+
+document.addEventListener('mousemove', (e) => {
+  const xRotation = e.pageY / 40 + 30;
+  const zRotation = e.pageX / 40 - 18;
+
+  perspectiveSquare.style.transform = `perspective(600px)
+                                       rotateX(${xRotation}deg)
+                                       rotateZ(${zRotation}deg)`;
+});
+
+perspectiveSquare.addEventListener('click', (e) => {
+  const POSSIBLE_COLORS = ['tomato', 'skyblue', 'lightgray', 'orange', 'black', 'yellow', 'yellowgreen'];
+  const possibleNextColors = POSSIBLE_COLORS.filter(
+    color => color != perspectiveSquare.style.backgroundColor
+  );
+  const randomColorIndex = Math.floor(Math.random() * possibleNextColors.length);
+
+  perspectiveSquare.style.backgroundColor = possibleNextColors[randomColorIndex];
+});
